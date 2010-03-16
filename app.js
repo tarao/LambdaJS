5cab8d095cd9e5f9855b96e3f287a3459a4000fa
tarao <tarao.gnn@gmail.com>
2010-03-16 05:02:03 +0000

    Clean.

--- app.js
+++ app.js
@@ -1,60 +1,3 @@
-function run(id) {
-    var code = document.getElementById(id);
-
-    var parser = new LambdaJS.Parser();
-    var parsed = parser.parse(code.textContent);
-    var sandbox = new LambdaJS.Sandbox();
-
-    var exp = sandbox.run(parsed.join(''));
-
-    code.appendChild(document.createElement('br'));
-    while (true) {
-        code.appendChild(document.createElement('br'));
-        code.appendChild(document.createTextNode((exp||'').toString()));
-        if (typeof exp == 'undefined') break;
-
-        var st = new LambdaJS.Strategy.CallByName();
-        exp = st.reduce(exp);
-        if (!st.reduced) break;
-    }
-
-    var exp = sandbox.run(parsed.join(''));
-
-    code.appendChild(document.createElement('br'));
-    while (true) {
-        code.appendChild(document.createElement('br'));
-        code.appendChild(document.createTextNode((exp||'').toString()));
-        if (typeof exp == 'undefined') break;
-
-        var st = new LambdaJS.Strategy.CallByValue();
-        exp = st.reduce(exp);
-        if (!st.reduced) break;
-    }
-
-    var exp = sandbox.run(parsed.join(''));
-
-    code.appendChild(document.createElement('br'));
-    while (true) {
-        code.appendChild(document.createElement('br'));
-        code.appendChild(document.createTextNode((exp||'').toString()));
-        if (typeof exp == 'undefined') break;
-
-        var st = new LambdaJS.Strategy.Leftmost();
-        exp = st.reduce(exp);
-        if (!st.reduced) break;
-    }
-};
-
-function initExample() {
-    var links = document.getElementsByTagName('a');
-    for (var i=0; i < links.length; i++) {
-        if (links[i].id.match(/^run-.+/)) {
-            var href = "javascript:run('$1')";
-            links[i].href = links[i].id.replace(/^run-(.+)$/, href);
-        }
-    }
-};
-
 if (typeof LambdaJS == 'undefined') var LambdaJS = {};
 if (typeof LambdaJS.App == 'undefined') LambdaJS.App = {};
 
@@ -217,8 +160,6 @@ function init(id) {
         // hide unsupported syntax
         hideSyntax(document.getElementById('syntax'),
                    isJS18Enabled() ? 'javascript' : 'javascript18');
-
-        initExample(); // FIXME
 
         // REPL
         var elm = document.getElementById(id);
